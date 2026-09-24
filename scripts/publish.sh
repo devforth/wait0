ae502cc9db3c68d5617c5fb5bf70e188f4c08f0b
Ivan Borshchov <ivan@devforth.io>
2026-09-24 11:21:59 +0000

    chore: bump wait0 release version to 1.4.2

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ docker buildx create --use
 docker buildx build \
 	--platform=linux/amd64,linux/arm64 \
 	--tag "devforth/wait0:latest" \
-	--tag "devforth/wait0:1.4.1" \
+	--tag "devforth/wait0:1.4.2" \
 	--push \
 	--file "$repo_root/Dockerfile" \
 	"$repo_root"
